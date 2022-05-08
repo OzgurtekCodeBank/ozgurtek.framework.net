5722a5cebf92146a095a485ff0ed4744528f92fb
enisozgur <enisozgur@gmail.com>
2022-05-08 15:23:27 +0300

    Mediadatasource test...

--- Framework/ozgurtek.framework.common/Data/Format/GdMediaDataSource.cs
+++ Framework/ozgurtek.framework.common/Data/Format/GdMediaDataSource.cs
@@ -17,48 +17,28 @@ namespace ozgurtek.framework.common.Data.Format
         /// Puts the given media....
         /// </summary>
         /// <example>hidrant\yeni\428\</example>
+        /// <example>hidrant\yeni\428\abc.jpg</example>
         /// <param name="stream">Byte stream of media file.</param>
         /// <param name="mediaPath"></param>       
         /// <param name="mediaType">.png</param>
         public void PutMedia(Stream stream, string mediaPath, string mediaType)
         {
-            //imagepath + file name -> dosya adı ile var mı diye kontrol edebilmek için
             string fullPath = Path.Combine(_basePath.OriginalString, mediaPath);
-            string extension = Path.GetExtension(mediaPath);
+            string extension = Path.GetExtension(fullPath);
 
-            string fullFilePath; //yazılacak dosyanın tam adresi
-            FileMode fileMode; //Dosya yazma modu(open ya da create)
-
-            if (!string.IsNullOrWhiteSpace(extension) && File.Exists(fullPath)) //Dosya var
+            string fullFilePath = fullPath;
+            if (string.IsNullOrEmpty(extension))//directory
             {
-                fullFilePath = Path.Combine(_basePath.OriginalString, mediaPath);
-                fileMode = FileMode.Open;
-            }
-            else //dosya yok
-            {
-                //Directoryleri yarat
-                string[] directories = mediaPath.Split('/');
-                fullFilePath = _basePath.OriginalString;
-                for (int i = 0; i < directories.Length; i++)
-                {
-                    fullFilePath = Path.Combine(fullFilePath, directories[i]);
-                    Directory.CreateDirectory(fullFilePath);
-                }
-
-                //dosya tipi
+                Directory.CreateDirectory(fullPath);
                 string ext = "." + mediaType.ToLowerInvariant();
-
-                //yaratılacak dosya adı
-                string generatedFileName =
-                    mediaPath.Replace("/", "_").Replace('\\', '_') + "-" + DateTime.Now.Ticks + ext;
-                fullFilePath = Path.Combine(fullFilePath, generatedFileName);
-                fileMode = FileMode.Create;
+                string fileName = mediaPath.Replace("/", "_").Replace('\\', '_') + "-" + DateTime.Now.Ticks + ext;
+                fullFilePath = Path.Combine(fullPath, fileName);
             }
 
-            FileStream targetStream = new FileStream(fullFilePath, fileMode, FileAccess.ReadWrite, FileShare.None);
-
+            string directoryName = Path.GetDirectoryName(fullFilePath);
+            Directory.CreateDirectory(directoryName);
+            FileStream targetStream = new FileStream(fullFilePath, FileMode.Create, FileAccess.ReadWrite, FileShare.None);
             stream.CopyTo(targetStream);
-
             targetStream.Close();
             targetStream.Dispose();
         }
@@ -105,11 +85,13 @@ namespace ozgurtek.framework.common.Data.Format
         /// <example>
         /// hidrant\yeni\428\
         /// hidrant\eski\125\
+        /// hidrant\eski\125\abc.jpg
         /// 
         /// returns
         /// hidrant_yeni_428_121545645454.png
         /// hidrant_yeni_428_12154564588989.word
         /// hidrant_yeni_428_121545454545454.mp4
+        /// hidrant_eski_125_abc.jpg
         /// </example>
         /// <param name="path">Path to get containing media files</param>
         /// <returns>
---
+++ Test/ozgurtek.framework.test.winforms/UnitTest/Driver/MediaDataSourceTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using NUnit.Framework;
+using ozgurtek.framework.common.Data.Format;
+
+namespace ozgurtek.framework.test.winforms.UnitTest.Driver
+{
+    [TestFixture]
+    public class MediaDataSourceTest
+    {
+        private string baseFolder = "C:\\Users\\eniso\\Desktop\\work\\testdata\\media";
+
+        [Test]
+        public void PutMediaWithFileNameTest()
+        {
+            GdMediaDataSource dataSource = new GdMediaDataSource(new Uri(baseFolder));
+            MemoryStream stream = new MemoryStream();
+            Image img = new Bitmap(Properties.Resources.Image1);
+            img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+            dataSource.PutMedia(stream, @"test/1/2/img.jpg", "jpg");
+        }
+
+        [Test]
+        public void PutMediaWithFolderNameTest()
+        {
+            GdMediaDataSource dataSource = new GdMediaDataSource(new Uri(baseFolder));
+            MemoryStream stream = new MemoryStream();
+            Image img = new Bitmap(Properties.Resources.Image1);
+            img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+            dataSource.PutMedia(stream, @"test/1/2", "jpg");
+        }
+
+        [Test]
+        public void GetMediaNamesTest()
+        {
+            GdMediaDataSource dataSource = new GdMediaDataSource(new Uri(baseFolder));
+            List<string> mediaNames = dataSource.GetMediaNames(@"test/1/2");
+            Assert.Greater(mediaNames.Count, 0);
+        }
+
+        [Test]
+        public void GetMediaTest()
+        {
+            GdMediaDataSource dataSource = new GdMediaDataSource(new Uri(baseFolder));
+            List<string> mediaNames = dataSource.GetMediaNames(@"test/1/2");
+            foreach (string mediaName in mediaNames)
+            {
+                string combine = Path.Combine(baseFolder, mediaName);
+                Stream inputStream = dataSource.GetMedia(mediaName);
+                using (FileStream outputFileStream = new FileStream(combine + "_output.jpg", FileMode.Create))
+                {
+                    inputStream.CopyTo(outputFileStream);
+                }
+            }
+        }
+
+        [Test]
+        public void DeleteMediaTest()
+        {
+            GdMediaDataSource dataSource = new GdMediaDataSource(new Uri(baseFolder));
+            List<string> mediaNames = dataSource.GetMediaNames(@"test/1/2");
+            foreach (string mediaName in mediaNames)
+            {
+                dataSource.DeleteMedia(mediaName);
+            }
+        }
+    }
+}

1ea42326b745e9739c19c09552ba3338b09db9fa
enisozgur <enisozgur@gmail.com>
2022-08-31 17:46:10 +0300

    abc

--- Framework/ozgurtek.framework.driver.gdal/Properties/AssemblyInfo.cs
+++ Framework/ozgurtek.framework.driver.gdal/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ozgurtek.framework.driver.gdal")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ozgurtek.framework.driver.gdal")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ozgurtek.framework.driver.gdal")]
 [assembly: AssemblyProduct("ozgurtek.framework.driver.gdal")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("ozgurtek.framework.driver.gdal")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible

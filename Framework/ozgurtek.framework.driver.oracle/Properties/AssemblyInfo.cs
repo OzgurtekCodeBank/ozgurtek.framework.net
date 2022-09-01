0fd6bb74dc338c7d06c31802eff93a0bcaf0bb0a
enisozgur <enisozgur@gmail.com>
2022-09-01 20:10:23 +0300

    2.0.55 relased....

--- Framework/ozgurtek.framework.driver.oracle/Properties/AssemblyInfo.cs
+++ Framework/ozgurtek.framework.driver.oracle/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.53.0")]
-[assembly: AssemblyFileVersion("2.0.53.0")]
+[assembly: AssemblyVersion("2.0.55.0")]
+[assembly: AssemblyFileVersion("2.0.55.0")]

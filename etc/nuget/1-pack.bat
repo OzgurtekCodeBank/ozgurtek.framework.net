af48fcf2bac6389ca12ae73183aa42fcb9dc45d1
enisozgur <enisozgur@gmail.com>
2023-03-23 22:40:17 +0300

    60 numaralı version

---
+++ etc/nuget/1-pack.bat
@@ -0,0 +1,20 @@
+nuget pack ..\..\Framework\ozgurtek.framework.core\ozgurtek.framework.core.csproj
+
+nuget pack ..\..\Framework\ozgurtek.framework.common\ozgurtek.framework.common.csproj
+
+nuget pack ..\..\Framework\ozgurtek.framework.driver.oracle\ozgurtek.framework.driver.oracle.csproj
+
+nuget pack ..\..\Framework\ozgurtek.framework.driver.oledb\ozgurtek.framework.driver.oledb.csproj
+
+nuget pack ..\..\Framework\ozgurtek.framework.driver.postgres\ozgurtek.framework.driver.postgres.csproj
+
+nuget pack ..\..\Framework\ozgurtek.framework.driver.sqlite\ozgurtek.framework.driver.sqlite.csproj
+
+nuget pack ..\..\Framework\ozgurtek.framework.driver.sqlserver\ozgurtek.framework.driver.sqlserver.csproj
+
+nuget pack ..\..\Framework\ozgurtek.framework.ui.controls.xamarin\ozgurtek.framework.ui.controls.xamarin.csproj
+
+nuget pack ..\..\Framework\ozgurtek.framework.ui.map.skiasharp\ozgurtek.framework.ui.map.skiasharp.csproj
+
+echo Press any key to Continue.
+pause
---
+++ etc/nuget/2-push.bat
@@ -0,0 +1,11 @@
+dotnet nuget push %cd%\ozgurtek.framework.core.2.0.60.nupkg --api-key oy2nh74damxfggyicewagth6owuhp32e5pty3i7jlddvei --skip-duplicate --source https://api.nuget.org/v3/index.json
+dotnet nuget push %cd%\ozgurtek.framework.common.2.0.60.nupkg --api-key oy2nh74damxfggyicewagth6owuhp32e5pty3i7jlddvei --skip-duplicate --source https://api.nuget.org/v3/index.json
+dotnet nuget push %cd%\ozgurtek.framework.driver.oledb.2.0.60.nupkg --api-key oy2nh74damxfggyicewagth6owuhp32e5pty3i7jlddvei --skip-duplicate --source https://api.nuget.org/v3/index.json
+dotnet nuget push %cd%\ozgurtek.framework.driver.postgres.2.0.60.nupkg --api-key oy2nh74damxfggyicewagth6owuhp32e5pty3i7jlddvei --skip-duplicate --source https://api.nuget.org/v3/index.json
+dotnet nuget push %cd%\ozgurtek.framework.driver.sqlite.2.0.60.nupkg --api-key oy2nh74damxfggyicewagth6owuhp32e5pty3i7jlddvei --skip-duplicate --source https://api.nuget.org/v3/index.json
+dotnet nuget push %cd%\ozgurtek.framework.ui.controls.xamarin.2.0.60.nupkg --api-key oy2nh74damxfggyicewagth6owuhp32e5pty3i7jlddvei --skip-duplicate --source https://api.nuget.org/v3/index.json
+dotnet nuget push %cd%\ozgurtek.framework.ui.map.skiasharp.2.0.60.nupkg --api-key oy2nh74damxfggyicewagth6owuhp32e5pty3i7jlddvei --skip-duplicate --source https://api.nuget.org/v3/index.json
+dotnet nuget push %cd%\ozgurtek.framework.driver.oracle.2.0.60.nupkg --api-key oy2nh74damxfggyicewagth6owuhp32e5pty3i7jlddvei --skip-duplicate --source https://api.nuget.org/v3/index.json
+dotnet nuget push %cd%\ozgurtek.framework.driver.sqlserver.2.0.60.nupkg --api-key oy2nh74damxfggyicewagth6owuhp32e5pty3i7jlddvei --skip-duplicate --source https://api.nuget.org/v3/index.json
+echo Press any key to Continue.
+pause

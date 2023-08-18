f1679c1df91d8cd945beeef78971d5f528248d5c
enisozgur <enisozgur@gmail.com>
2023-08-18 15:23:23 +0300

    aaa

--- Test/ozgurtek.framework.test.winforms/UnitTest/Driver/GdalDriverTest.cs
+++ Test/ozgurtek.framework.test.winforms/UnitTest/Driver/GdalDriverTest.cs
@@ -4,7 +4,11 @@ using System.Collections.Generic;
 using System.Drawing;
 using NetTopologySuite.Geometries;
 using OSGeo.GDAL;
+using ozgurtek.framework.common.Data;
 using ozgurtek.framework.common.Geodesy;
+using ozgurtek.framework.core.Data;
+using ozgurtek.framework.driver.sqlite;
+using Point = NetTopologySuite.Geometries.Point;
 
 namespace ozgurtek.framework.test.winforms.UnitTest.Driver
 {
@@ -53,23 +57,39 @@ namespace ozgurtek.framework.test.winforms.UnitTest.Driver
         [Test]
         public void ExportTest()
         {
+            GdSqlLiteDataSource sqlLiteDataSource = GdSqlLiteDataSource.OpenOrCreate("C:\\Users\\eniso\\Desktop\\height.sqlite");
+            GdSqlLiteTable liteTable = sqlLiteDataSource.CreateTable("height", GdGeometryType.Point, 4326, null);
+            liteTable.CreateField(new GdField("x", GdDataType.Real));
+            liteTable.CreateField(new GdField("y", GdDataType.Real));
+            liteTable.CreateField(new GdField("height", GdDataType.Real));
+            liteTable.CreateField(new GdField("str", GdDataType.Real));
+
             GdGdalDataSource dataSource = GdGdalDataSource.Open(_path);
             int w = dataSource.RasterWidth;
             int h = dataSource.RasterHeight;
 
-            List<string> result = new List<string>();
-            Coordinate unProject = dataSource.UnProject(579075.6, 4187474.6);
-            for (int i = 0; i < w; i++)
+            int density = 100;
+            sqlLiteDataSource.BeginTransaction();
+            for (int i = 0; i < w; i += density)
             {
-                for (int j = 0; j < h; j++)
+                for (int j = 0; j < h; j += density)
                 {
                     double pixelVal = dataSource.ReadBand(1, i, j);
                     Coordinate coordinate = dataSource.Project(i, j);
-                    result.Add($"{coordinate.X}-{coordinate.Y}-{pixelVal}" );
+                    Coordinate project = GdProjection.Project(coordinate, 5253, 4326);
+                    Point point = new Point(project);
+                    point.SRID = 4326;
+
+                    GdRowBuffer buffer = new GdRowBuffer();
+                    buffer.Put("geometry", point);
+                    buffer.Put("x", project.X);
+                    buffer.Put("y", project.Y);
+                    buffer.Put("height", pixelVal);
+                    buffer.Put("str", $"{coordinate.X}-{coordinate.Y}-{pixelVal}");
+                    liteTable.Insert(buffer);
                 }
             }
-
-            string xtx = string.Join(" ", result);
+            sqlLiteDataSource.CommitTransaction();
         }
 
     }

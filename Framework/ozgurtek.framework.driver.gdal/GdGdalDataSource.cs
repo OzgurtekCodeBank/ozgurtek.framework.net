1107ba988822dd2fe8e3340a6f7352a45eb91a62
enisozgur <enisozgur@gmail.com>
2023-08-18 11:02:04 +0300

    gdal impelemntation....

---
+++ Framework/ozgurtek.framework.driver.gdal/GdGdalDataSource.cs
@@ -0,0 +1,38 @@
+﻿using OSGeo.GDAL;
+using System.Collections.Generic;
+
+namespace ozgurtek.framework.driver.gdal
+{
+    public class GdGdalDataSource
+    {
+        internal GdGdalDataSource()
+        {
+        }
+
+        public static GdOgrDataSource Open(string source, bool editable = false)
+        {
+            GdalConfiguration.ConfigureGdal();
+            int count = Gdal.GetDriverCount();
+            for (int i = 0; i < count; i++)
+            {
+                OSGeo.GDAL.Driver driver = Gdal.GetDriver(i);
+            }
+
+            return null;
+        }
+
+        public static IEnumerable<string> DriverNames
+        {
+            get
+            {
+                GdalConfiguration.ConfigureGdal();
+                int count = Gdal.GetDriverCount();
+                for (int i = 0; i < count; i++)
+                {
+                    Driver driver = Gdal.GetDriver(i);
+                    yield return driver.ShortName + " " + driver.LongName + " " + driver.GetDescription();
+                }
+            }
+        }
+    }
+}
---
+++ Test/ozgurtek.framework.test.winforms/UnitTest/Driver/GdalDriverTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using ozgurtek.framework.driver.gdal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ozgurtek.framework.test.winforms.UnitTest.Driver
+{
+    [TestFixture]
+    public class GdalDriverTest
+    {
+
+        [Test]
+        public void DriverTest()
+        {
+            IEnumerable<string> driverNames = GdGdalDataSource.DriverNames;
+            List<string> driverName = new List<string>(driverNames);
+            Assert.GreaterOrEqual(driverName.Count, 1);
+        }
+    }
+}

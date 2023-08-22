cd0e923bfdadc84708ae7ef2467b6df142234f5d
enisozgur <enisozgur@gmail.com>
2023-08-22 12:24:46 +0300

    31 level yazıldı googlematrixset de

---
+++ Test/ozgurtek.framework.test.winforms/UnitTest/Util/TileMatrixSetTest.cs
@@ -0,0 +1,26 @@
+﻿using NetTopologySuite.Geometries;
+using NUnit.Framework;
+using ozgurtek.framework.common.Data.Format.OnlineMap.Google;
+using ozgurtek.framework.common.Util;
+using ozgurtek.framework.core.Data;
+using ozgurtek.framework.driver.gdal;
+using System.Collections.Generic;
+
+namespace ozgurtek.framework.test.winforms.UnitTest.Util
+{
+    [TestFixture]
+    public class TileMatrixSetTest
+    {
+        [Test]
+        public void LevelTest1()
+        {
+            GdGoogleMapsTileMatrixSet set = new GdGoogleMapsTileMatrixSet();
+            GdTileIndex index = new GdTileIndex(302760, 202569, 19);
+            GdTileMatrixCalculator cal = new GdTileMatrixCalculator(set);
+            Polygon pol = cal.GetGeometry(index);
+            Envelope polEnvelopeInternal = pol.EnvelopeInternal;
+            Assert.Greater(pol.Area, 0);
+        }
+
+    }
+}
